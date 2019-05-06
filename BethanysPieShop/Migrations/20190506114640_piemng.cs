5e592e97c6126c8495045e13ded880079c24f8cd
Essam <Essam.3ali@gmail.com>
2019-05-06 14:57:51 +0300

    vali

---
+++ BethanysPieShop/Migrations/20190506114640_piemng.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class piemng : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Pies",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SugarLevel",
+                table: "Pies",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "RecipeInformation",
+                columns: table => new
+                {
+                    RecipeInformationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PreparationDirections = table.Column<string>(nullable: true),
+                    Duration = table.Column<int>(nullable: false),
+                    PieId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeInformation", x => x.RecipeInformationId);
+                    table.ForeignKey(
+                        name: "FK_RecipeInformation_Pies_PieId",
+                        column: x => x.PieId,
+                        principalTable: "Pies",
+                        principalColumn: "PieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeInformation_PieId",
+                table: "RecipeInformation",
+                column: "PieId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecipeInformation");
+
+            migrationBuilder.DropColumn(
+                name: "SugarLevel",
+                table: "Pies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Pies",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
--- BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/Models/Pie.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Utility;
 
 namespace BethanysPieShop.Models
 {
@@ -9,17 +11,28 @@ namespace BethanysPieShop.Models
     {
         public int PieId { get; set; }
         public string Name { get; set; }
+
+        [MaxLength(100)]
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
         public string AllergyInformation { get; set; }
         public decimal Price { get; set; }
+
+        [ValidUrl(ErrorMessage = "That's not a valid URL")]
         public string ImageUrl { get; set; }
+
+        [ValidUrl(ErrorMessage = "That's not a valid URL")]
         public string ImageThumbnailUrl { get; set; }
         public bool IsPieOfTheWeek { get; set; }
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
         public virtual List<PieReview> PieReviews { get; set; }
+
+        //Specific for Model Binding
+        public SugarLevel SugarLevel { get; set; }
+        public RecipeInformation RecipeInformation { get; set; }
+
 
     }
 }
